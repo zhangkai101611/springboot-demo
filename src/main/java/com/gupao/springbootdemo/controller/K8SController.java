b518f42b4cb36f5f4f3f865819d3d2c985337517
itcrazy2016 <itcrazy2016@163.com>
2019-10-21 03:55:50 +0800

    first commit

---
+++ src/main/java/com/gupao/springbootdemo/controller/K8SController.java
@@ -0,0 +1,13 @@
+package com.gupao.springbootdemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class K8SController {
+
+    @RequestMapping("/k8s")
+    public String k8s(){
+        return "hello k8s!";
+    }
+}
