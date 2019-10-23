2330055d610ae7da43f2831a9806f652b8a277e7
itcrazy2016 <itcrazy2016@163.com>
2019-10-23 20:09:14 +0800

    Update K8SController.java

--- src/main/java/com/gupao/springbootdemo/controller/K8SController.java
+++ src/main/java/com/gupao/springbootdemo/controller/K8SController.java
@@ -8,6 +8,6 @@ public class K8SController {
 
     @RequestMapping("/k8s")
     public String k8s(){
-        return "hello k8s update aa1a1 gupao haha nice,I'm Jack!";
+        return "hello k8s update aa1a1 gupao haha nice,I'm Jack!go";
     }
 }
