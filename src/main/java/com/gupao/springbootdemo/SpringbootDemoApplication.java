8b7abaf41d71dca01bf9d6bc91d9680daf5344f8
itcrazy2016 <itcrazy2016@163.com>
2019-11-15 19:51:05 +0800

    jack

--- src/main/java/com/gupao/springbootdemo/SpringbootDemoApplication.java
+++ src/main/java/com/gupao/springbootdemo/SpringbootDemoApplication.java
@@ -3,10 +3,14 @@ package com.gupao.springbootdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @SpringBootApplication
 public class SpringbootDemoApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnknownHostException {
+
         SpringApplication.run(SpringbootDemoApplication.class, args);
     }
 
--- src/main/java/com/gupao/springbootdemo/controller/K8SController.java
+++ src/main/java/com/gupao/springbootdemo/controller/K8SController.java
@@ -3,11 +3,25 @@ package com.gupao.springbootdemo.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+
 @RestController
 public class K8SController {
 
+    String result="";
+
     @RequestMapping("/k8s")
     public String k8s(){
-        return "hello k8s update aa1a1 gupao haha nice,I'm Jack!go";
+        try {
+            //用 getLocalHost() 方法创建的InetAddress的对象
+            InetAddress address = InetAddress.getLocalHost();
+            result="hostname: "+address.getHostName()+"hostaddress: "+address.getHostAddress();
+            System.out.println();//主机名
+            System.out.println();//主机别名
+            System.out.println();//
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return "hello K8s <br/> "+result;
     }
 }
